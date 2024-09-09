d93b73f3a878014da23e09837af65d76a5dbe7ca
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-09-09 13:39:51 -0700

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div></div>
+      <div>MUSIC</div>
+      <div>MUSIC REVIEWS</div>
+      <div>SHOOTS</div>
+      <div>POEMS</div>
+      <div>CONTACT</div>
+    </div>
+  );
+};
+
+export default Navbar;
