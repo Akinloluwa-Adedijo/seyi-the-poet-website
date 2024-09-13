78d1d228d06154094ce54afd88dcff80d5f7ccd0
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-09-12 22:21:28 -0700

    fixed nav styling and navbar structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,49 +1,31 @@
 import { useState } from "react";
 import "./navbar.css";
 import { motion } from "framer-motion";
-
-const opacityVariants = {
-  initial: {
-    opacity: 0,
-  },
-  open: {
-    opacity: 1,
-    transition: { duration: 0.35 },
-  },
-  closed: {
-    opacity: 0,
-    transition: { duration: 0.35 },
-  },
-};
+import { opacity } from "../animationVariants";
 
 const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
   return (
-    <nav className="navbar">
-      <div className="nav-title">
-        <p>Sèyí,ThePoet</p>
-      </div>
-
-      <div
-        onClick={() => {
-          setIsActive(!isActive);
-        }}
-        className="nav-menu"
-      >
-        <motion.p
-          variants={opacityVariants}
-          animate={!isActive ? "open" : "closed"}
+    <div className="nav-header">
+      <div className="bar">
+        <div className="nav-title">
+          <p>Sèyí,ThePoet</p>
+        </div>
+        <div
+          onClick={() => {
+            setIsActive(!isActive);
+          }}
+          className="nav-menu-btn"
         >
-          Menu
-        </motion.p>
-        <motion.p
-          variants={opacityVariants}
-          animate={isActive ? "open" : "closed"}
-        >
-          Close
-        </motion.p>
+          <motion.p variants={opacity} animate={!isActive ? "open" : "closed"}>
+            Menu
+          </motion.p>
+          <motion.p variants={opacity} animate={isActive ? "open" : "closed"}>
+            Close
+          </motion.p>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
 
