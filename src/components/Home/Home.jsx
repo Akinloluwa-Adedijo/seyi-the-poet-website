84d301ee2fd356ebfdff2fdcb8fc2f8edfe2d905
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-09-18 21:48:51 -0700

    added home page structure

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,23 @@
 import { motion } from "framer-motion";
 import "./home.css";
+import { useRef } from "react";
 
 import Transition from "../Transition/Transition";
 const Home = () => {
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
   return (
     <div className="home">
       <div className="landing-page">
         <img src="/src/assets/home.webp" alt="" />
-        <p>Sèyí,ThePoet</p>
+        {/* <p>Sèyí,ThePoet</p> */}
+        <div className="slider-container">
+          <div className="slider">
+            <p>Artist -</p>
+            <p>Artist -</p>
+          </div>
+        </div>
       </div>
     </div>
   );
