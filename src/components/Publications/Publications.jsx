3712f17c13ecae356dc4d3ecd7c19d472a4c8fc1
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-09-24 14:03:11 -0700

    added publication section

--- src/components/Publications/Publications.jsx
+++ src/components/Publications/Publications.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { animate, motion, useInView } from "framer-motion";
 import "./publications.css";
 
 const publications = [
@@ -28,21 +29,64 @@ const publications = [
   },
 ];
 
-const Publications = () => {
+const publicationVariants = {
+  initial: { width: 0 },
+  open: {
+    width: "auto",
+    transition: { duration: 0.5, ease: [0.65, 0, 0.35, 1] },
+  },
+  closed: { width: 0 },
+};
+
+const landingTitleVariants = {
+  hidden: { opacity: 0, x: 200 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { duration: 1, ease: [0.65, 0, 0.35, 1] },
+  },
+};
+
+const Publication = ({ data }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const { title1, title2, link, src } = data;
   return (
-    <div className="pubContainer">
-      <div className="pubGallery">
-        <p>Publications</p>
-        {publications.map((pub, index) => {
-          return (
-            <div key={index} className="publications">
-              <p>{pub.title1}</p>
-              <div className="pubImgContainer">
-                <img src={pub.src} alt="Publication Card Image" />
-              </div>
-              <p>{pub.title2}</p>
-            </div>
-          );
+    <div
+      className="publication"
+      onMouseEnter={() => {
+        setIsActive(true);
+      }}
+      onMouseLeave={() => {
+        setIsActive(false);
+      }}
+    >
+      <p>{title1}</p>
+      <motion.div
+        variants={publicationVariants}
+        animate={isActive ? "open" : "closed"}
+        className="publication-image-container"
+      >
+        <img src={src} alt="Publication Card Image" />
+      </motion.div>
+      <p>{title2}</p>
+    </div>
+  );
+};
+const Publications = () => {
+  const tRef = useRef();
+  const inView = useInView(tRef);
+  return (
+    <div className="publication-container">
+      <div className="publication-gallery" ref={tRef}>
+        <motion.h2
+          variants={landingTitleVariants}
+          animate={inView ? "visible" : "hidden"}
+        >
+          Publications
+        </motion.h2>
+        {publications.map((data, index) => {
+          return <Publication data={data} key={index} />;
         })}
       </div>
     </div>
